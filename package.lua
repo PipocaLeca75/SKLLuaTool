f8ebcdff23c38192d745aea72602cab0bec8ce1c
dronatio <pipocapaloca@gmail.com>
2025-08-21 16:54:02 -0300

    Done
    First commit

---
+++ package.lua
@@ -0,0 +1,12 @@
+  return {
+    name = "SKLLuaTool",
+    version = "1.0.0",
+    description = "compilador e interpretador de SKL",
+    tags = { "SKL", "lua", "tool" },
+    license = "MIT",
+    author = { name = "PipocaLeca75" },
+    homepage = "https://github.com/PipocaLeca75/SKLLuaTool",
+    dependencies = {},
+    files = {"**.lua"}
+  }
+  
